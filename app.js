9d72be1851f5b3f55b6c365bd14061adb723f4a6
Milos Ilic <milosilic89@outlook.com>
2023-09-22 11:46:39 +0200

    Installed dependencies

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const Web3 = require('web3');
+
+const app = express();
+const port = 3000;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
